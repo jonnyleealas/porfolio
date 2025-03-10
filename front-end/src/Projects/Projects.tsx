a5c8355b10ecc88f51410cb9223c0d8d675530c9
jonathon lee <jonnyleealas@gmail.com>
2025-03-10 01:08:19 -0500

    Update Projects.tsx

--- front-end/src/Projects/Projects.tsx
+++ front-end/src/Projects/Projects.tsx
@@ -1,36 +1,20 @@
-const Projects: React.FC = () => {
-    return (
-      <div>
-        <h1>Projects</h1>
-        <h1>Projects</h1>
-        <h1>Projects</h1>
-        <h1>Projects</h1>
-        <h1>Projects</h1>
-        <h1>Projects</h1>
-        <h1>Projects</h1>
-        <h1>Projects</h1>
-        <h1>Projects</h1>
-        <h1>Projects</h1>
-        <h1>Projects</h1>
-        <h1>Projects</h1>
-        <h1>Projects</h1>
-        <h1>Projects</h1>
-        <h1>Projects</h1>
-        <h1>Projects</h1>
-        <h1>Projects</h1>
-        <h1>Projects</h1>
-        <h1>Projects</h1>
-        <h1>Projects</h1>
-        <h1>Projects</h1>
-        <h1>Projects</h1>
-        <h1>Projects</h1>
-        <h1>Projects</h1>
-        <h1>Projects</h1>
-        <h1>Projects</h1>
-        <h1>Projects</h1>
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
-      </div>
-    );
-  }
-  
-  export default Projects;
+const Projects: React.FC = () => {
+  return (
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default Projects;
